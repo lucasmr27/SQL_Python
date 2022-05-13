931e5d9750389c541f3b1aa17a9649ae1281427b
Lucas Rocha <lucas_rocha27@hotmail.com>
2022-05-12 22:15:00 -0300

    Primeiro commit aula-7

---
+++ README.md
@@ -0,0 +1,10 @@
+Estudos do curso SQL com Python do canal DATA VIKING no YouTube
+
+# Link com os códigos
+https://colab.research.google.com/drive/1NS7dXE3VEpUFhUbP6dNn_jVm8J3bwRym?usp=sharing
+
+# Documentação SQLite3
+https://docs.python.org/3/library/sqlite3.html
+
+Instalação da biblioteca
+pip install db-sqlite3
---
+++ main.py
@@ -0,0 +1,34 @@
+import sqlite3
+
+# Criando Banco de Dados
+Conexao = sqlite3.connect('Banco_Dados')
+
+# Apontar para o banco
+Cursor = Conexao.cursor()
+
+# Criando uma tabela
+# Cursor.execute(
+#     'CREATE TABLE Minha_Tabela ( Data text, Nome text, Idade real )'
+# )
+
+# Fazer um commit
+Conexao.commit()
+
+# Inserir valores
+Cursor.execute('INSERT INTO Minha_Tabela VALUES ( "27/01/2021", "Lucas", "30" )')
+Cursor.execute('INSERT INTO Minha_Tabela VALUES ( "01/01/2021", "Odemir", "30" )')
+
+import random
+
+# Loop
+for loop in range(10):
+
+    # Gerando numeros aleatorios
+    num = random.randint(18, 65)
+    dia = random.randint(1, 28)
+    mes = random.randint(1, 12)
+    Cursor.execute(f'INSERT INTO Minha_Tabela VALUES ( "{dia}/{mes}/2021", "Lucas", {num} )')
+
+# Query de consultas
+Consulta = Cursor.execute(' SELECT * FROM Minha_Tabela').fetchall()
+print(Consulta)
